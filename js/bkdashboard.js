596667837d7d4799428ba4b4c15f2d1ee79f0415
Björn Kämmereit <bjoern.kaemmereit@project-collins.com>
2017-10-15 23:26:30 +0200

    Cadence arc with average value calculation added

--- js/bkdashboard.js
+++ js/bkdashboard.js
@@ -5,7 +5,7 @@ class BkDashboard
      */
     constructor()
     {
-        this.version = '0.0.3';
+        this.version = '0.0.4';
         
         this.localStorageAvailable =
             (
@@ -15,22 +15,40 @@ class BkDashboard
         this.stageWidth = $(window).width();
         this.stageHeight = $(window).height();
         
-        this.arcWidth = this.stageWidth / 20;
+        this.speedArcWidth = this.stageWidth / 20;
         
-        this.arcInnerRadius = this.stageWidth / 2.5;
-        this.arcOuterRadius = this.arcInnerRadius - this.arcWidth;
+        this.speedArcInnerRadius = this.stageWidth / 2.5;
+        this.speedArcOuterRadius = this.speedArcInnerRadius - this.speedArcWidth;
+        
+        this.cadenceArcInnerRadius = this.speedArcInnerRadius - 55;
+        this.cadenceArcOuterRadius = this.speedArcOuterRadius - 25;
+        
         this.arcX = 0;
         this.arcY = 0;
         
+        this.acrToBackground = 5;
+        
         this.maxSpeed = 0;
         this.maxSpeedCeiling = 20;
-        this.maxSpeedDefault = 20;
         
         this.averageSpeed = 0;
+        this.averageCadence = 0;
         
         this.lastSpeedUpdate = 0;
         this.checkSpeedInterval = 1000;
         this.checkSpeedIntervalObject = null;
+        
+        this.lastCadenceUpdate = 0;
+        this.checkCadenceInterval = 1000;
+        this.checkCadenceIntervalObject = null;
+        
+        this.averageCadenceInterval = 1000;
+        this.averageCadenceIntervalObject = null;
+        
+        this.maxCadence = 0;
+        this.maxCadenceCeiling = 50;
+        
+        this.lastCadenceUpdate = 0;
     }
     
     /**
@@ -69,8 +87,8 @@ class BkDashboard
                 {
                     x:           this.arcX,
                     y:           this.arcY,
-                    innerRadius: this.arcInnerRadius + 5,
-                    outerRadius: this.arcOuterRadius - 5,
+                    innerRadius: this.speedArcInnerRadius + this.acrToBackground,
+                    outerRadius: this.speedArcOuterRadius - this.acrToBackground,
                     angle:       180,
                     fill:        '#444353',
                     rotation:    180
@@ -82,8 +100,8 @@ class BkDashboard
                 {
                     x:             this.arcX,
                     y:             this.arcY,
-                    innerRadius:   this.arcInnerRadius,
-                    outerRadius:   this.arcOuterRadius,
+                    innerRadius:   this.speedArcInnerRadius,
+                    outerRadius:   this.speedArcOuterRadius,
                     angle:         180,
                     fill:          '#00C8E6',
                     shadowEnabled: true,
@@ -97,14 +115,58 @@ class BkDashboard
                 {
                     x:             this.arcX,
                     y:             this.arcY,
-                    innerRadius:   this.arcInnerRadius + 15,
-                    outerRadius:   this.arcOuterRadius,
+                    innerRadius:   this.speedArcInnerRadius + this.acrToBackground + 10,
+                    outerRadius:   this.speedArcOuterRadius - this.acrToBackground,
                     angle:         2,
                     opacity:       0.9,
                     fill:          '#FFF',
                     shadowEnabled: true,
                     shadowBlur:    10,
                     shadowColor:   '#FFF'
+                }
+            );
+        
+        this.averageCadenceArc =
+            new Konva.Arc(
+                {
+                    x:             this.arcX,
+                    y:             this.arcY,
+                    innerRadius:   this.cadenceArcInnerRadius + this.acrToBackground,
+                    outerRadius:   this.cadenceArcOuterRadius - this.acrToBackground,
+                    angle:         2,
+                    opacity:       0.9,
+                    fill:          '#FFF',
+                    shadowEnabled: true,
+                    shadowBlur:    10,
+                    shadowColor:   '#FFF'
+                }
+            );
+        
+        this.cadenceArcBackground =
+            new Konva.Arc(
+                {
+                    x:           this.arcX,
+                    y:           this.arcY,
+                    innerRadius: this.cadenceArcInnerRadius + this.acrToBackground,
+                    outerRadius: this.cadenceArcOuterRadius - this.acrToBackground,
+                    angle:       180,
+                    fill:        '#444353',
+                    rotation:    180
+                }
+            );
+        
+        this.curCadenceArc =
+            new Konva.Arc(
+                {
+                    x:             this.arcX,
+                    y:             this.arcY,
+                    innerRadius:   this.cadenceArcInnerRadius,
+                    outerRadius:   this.cadenceArcOuterRadius,
+                    angle:         180,
+                    fill:          '#00C8E6',
+                    shadowEnabled: true,
+                    shadowBlur:    10,
+                    shadowColor:   '#00C8E6'
                 }
             );
         
@@ -135,9 +197,13 @@ class BkDashboard
             );
         
         this.mask.add(this.speedArcBackground);
+        this.mask.add(this.cadenceArcBackground);
         
         this.mask.add(this.curSpeedArc);
         this.mask.add(this.averageSpeedArc);
+        
+        this.mask.add(this.curCadenceArc);
+        this.mask.add(this.averageCadenceArc);
         
         this.layer.add(this.mask);
         this.layer.add(this.bottomBar);
@@ -147,11 +213,16 @@ class BkDashboard
         $('#version').text(this.version);
     }
     
+    /**
+     * @param  speed
+     */
     updateCurrentSpeedView(speed)
     {
         this.lastSpeedUpdate = Date.now().toString();
         
         speed = this.round(speed);
+        
+        this.setMaxSpeed(speed);
         
         let opacity = 1;
         if (speed === 0)
@@ -172,15 +243,47 @@ class BkDashboard
         
         tween.play();
         
-        this.setMaxSpeed(speed);
-        
         $('#curSpeed').text(speed);
         $('#maxSpeed').text(this.maxSpeedCeiling);
+    }
+    
+    updateCurrentCadenceView(cadence)
+    {
+        this.lastCadenceUpdate = Date.now().toString();
+        
+        cadence = this.round(cadence);
+        
+        this.setMaxCadence(cadence);
+        this.setAvrCadence(cadence);
+        
+        let opacity = 1;
+        if (cadence === 0)
+        {
+            opacity = 0;
+        }
+        
+        let tween =
+            new Konva.Tween(
+                {
+                    node:     this.curCadenceArc,
+                    rotation: this.calculateCadenceBasedRotation(cadence),
+                    easing:   Konva.Easings.EaseInOut,
+                    duration: 0.5,
+                    opacity:  opacity
+                }
+            );
+        
+        tween.play();
+        
+        $('#curCadence').text(cadence);
+        $('#maxCadence').text(this.maxCadenceCeiling);
     }
     
     updateAverageSpeedView(speed, initial = false)
     {
         speed = this.round(speed);
+        
+        this.setAvrSpeed(speed);
         
         let duration = 0.5;
         if (initial === true)
@@ -206,15 +309,60 @@ class BkDashboard
         
         tween.play();
         
-        this.setAvrSpeed(speed);
-        
         $('#avrSpeed').text(speed);
     }
     
-    initAverageView()
+    updateAverageCadenceView(cadence, initial = false)
+    {
+        cadence = this.round(cadence);
+        
+        this.setAvrCadence(cadence);
+        
+        let duration = 0.5;
+        if (initial === true)
+        {
+            duration = 5;
+        }
+        
+        let rotation = 0;
+        if (cadence !== 0)
+        {
+            rotation = this.calculateCadenceBasedRotation(cadence) + 180;
+        }
+        
+        let tween =
+            new Konva.Tween(
+                {
+                    node:     this.averageCadenceArc,
+                    rotation: rotation,
+                    easing:   Konva.Easings.EaseInOut,
+                    duration: duration,
+                }
+            );
+        
+        tween.play();
+        
+        $('#avrCadence').text(cadence);
+    }
+    
+    initAverageSpeedView()
     {
         this.setMaxSpeed(this.getAvrSpeed());
         this.updateAverageSpeedView(this.getAvrSpeed(), true);
+    }
+    
+    initAverageCadenceView()
+    {
+        this.setMaxCadence(this.getAvrCadence());
+        this.updateAverageCadenceView(this.getAvrCadence(), true);
+        
+        if (this.averageCadenceIntervalObject === null)
+        {
+            this.averageCadenceIntervalObject =
+                setInterval(() => {
+                    this.updateAverageCadenceView(this.getAvrCadence(), false);
+                }, this.averageCadenceInterval);
+        }
     }
     
     initSpeedCheck()
@@ -232,10 +380,34 @@ class BkDashboard
         }
     }
     
+    initCadenceCheck()
+    {
+        if (this.checkCadenceIntervalObject === null)
+        {
+            this.checkCadenceIntervalObject =
+                setInterval(() => {
+                    if (Date.now().toString() - this.lastCadenceUpdate >
+                        this.checkCadenceInterval)
+                    {
+                        this.updateCurrentCadenceView(0);
+                    }
+                }, this.checkCadenceInterval);
+        }
+    }
+    
     calculateSpeedBasedRotation(speed)
     {
         let factor = (
             100 / this.maxSpeedCeiling) * speed;
+        
+        return (
+            180 / 100) * factor;
+    }
+    
+    calculateCadenceBasedRotation(cadence)
+    {
+        let factor = (
+            100 / this.maxCadenceCeiling) * cadence;
         
         return (
             180 / 100) * factor;
@@ -254,20 +426,60 @@ class BkDashboard
         }
     }
     
+    setAvrCadence(cadence)
+    {
+        if (cadence > 0)
+        {
+            
+            cadence =
+                (
+                    (
+                        this.averageCadence * 99) + cadence) / 100;
+            
+            cadence = this.round(cadence);
+            
+            if (this.averageCadence !== cadence)
+            {
+                this.averageCadence = cadence;
+                
+                if (this.localStorageAvailable === true)
+                {
+                    localStorage.setItem('averageCadence', this.averageCadence);
+                }
+            }
+        }
+    }
+    
     getAvrSpeed()
     {
         if (this.localStorageAvailable === true)
         {
-            let speed = localStorage.getItem('averageSpeed');
+            let value = localStorage.getItem('averageSpeed');
             
             if (typeof(
-                    speed) !== 'undefined')
+                    value) !== 'undefined')
             {
-                this.averageSpeed = speed;
+                this.averageSpeed = value;
             }
         }
         
         return this.averageSpeed;
+    }
+    
+    getAvrCadence()
+    {
+        if (this.localStorageAvailable === true)
+        {
+            let value = localStorage.getItem('averageCadence');
+            
+            if (typeof(
+                    value) !== 'undefined')
+            {
+                this.averageCadence = value;
+            }
+        }
+        
+        return this.averageCadence;
     }
     
     setMaxSpeed(speed)
@@ -286,24 +498,20 @@ class BkDashboard
         }
     }
     
-    getMaxSpeedCeiling()
+    setMaxCadence(cadence)
     {
-        if (this.localStorageAvailable === true)
+        if (cadence > this.maxCadenceCeiling)
         {
-            let speed = localStorage.getItem('maxSpeedCeiling');
+            this.maxCadence = this.round(cadence);
+            this.maxCadenceCeiling = Math.ceil(this.maxCadence / 10) * 10;
             
             if (typeof(
-                    speed) !== 'undefined')
+                    Storage) !== 'undefined')
             {
-                this.maxSpeedCeiling = speed;
-            }
-            else
-            {
-                this.maxSpeedCeiling = this.maxSpeedDefault;
+                localStorage.setItem('maxCadence', this.maxCadence);
+                localStorage.setItem('maxCadenceCeiling', this.maxCadenceCeiling);
             }
         }
-        
-        return this.maxSpeedCeiling;
     }
     
     round(value)
